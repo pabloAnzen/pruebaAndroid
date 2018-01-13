7265b0f21151c37e971f4d5ee504845a70ba98f6
pabloAnzen <ptovar@anzen.com.mx>
2018-01-12 19:01:15 -0600

    Inicio proyecto

---
+++ app/src/main/java/com/pablocasvar/aplicaciona/MainActivity.java
@@ -0,0 +1,18 @@
+package com.pablocasvar.aplicaciona;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText primerInput;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        private String valor = 
+    }
+}
---
+++ justJava/app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.android.justjava;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void submitOrder(View view){
+        display(1);
+    }
+
+    private void display(int number){
+        TextView quantityTextView = (TextView) findViewById(R.id.quantity_text_view);
+        quantityTextView.setText("" + number);
+    }
+}
