6a286d62619e4107cfebbc0a10422fcdc9852ac7
pabloAnzen <ptovar@anzen.com.mx>
2018-01-15 11:34:19 -0600

    Vista modificada, funcionalidad de calculo de precio agregada

--- justJava/app/src/main/java/com/example/android/justjava/MainActivity.java
+++ justJava/app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -5,20 +5,49 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 
+import org.w3c.dom.Text;
+
+import java.text.NumberFormat;
+
 public class MainActivity extends AppCompatActivity {
+
+    private TextView quantityTextView;
+    private TextView priceTextView;
+    private int quantity = 0;
+    private static final double PRICE_PER_COFFE = 5;
+    private double price = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        quantityTextView    = (TextView) findViewById(R.id.quantity_text_view);
+        priceTextView       = (TextView) findViewById(R.id.price_text_view);
+
     }
 
     public void submitOrder(View view){
-        display(1);
+        price = (double) quantity * PRICE_PER_COFFE;
+        displayPrice(price);
     }
 
     private void display(int number){
-        TextView quantityTextView = (TextView) findViewById(R.id.quantity_text_view);
         quantityTextView.setText("" + number);
+    }
+
+    private void displayPrice(double price){
+        priceTextView.setText(NumberFormat.getCurrencyInstance().format(price));
+    }
+
+    public void increment(View view){
+        quantity++;
+        display(quantity);
+    }
+
+    public  void decrement(View view){
+        if (quantity > 1)
+            quantity--;
+        display(quantity);
     }
 }
