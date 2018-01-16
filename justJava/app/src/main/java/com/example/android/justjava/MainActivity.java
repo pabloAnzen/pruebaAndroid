5f0473f367dbf69d96b0751d6787720d43c1a532
pabloAnzen <ptovar@anzen.com.mx>
2018-01-16 11:25:57 -0600

    Prueba herencia MainLayout

--- justJava/app/src/main/java/com/example/android/justjava/MainActivity.java
+++ justJava/app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -5,14 +5,12 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 
-import org.w3c.dom.Text;
-
 import java.text.NumberFormat;
 
 public class MainActivity extends AppCompatActivity {
 
     private TextView quantityTextView;
-    private TextView priceTextView;
+    private TextView orderSummaryTextView;
     private int quantity = 0;
     private static final double PRICE_PER_COFFE = 5;
     private double price = 0;
@@ -23,31 +21,43 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         quantityTextView    = (TextView) findViewById(R.id.quantity_text_view);
-        priceTextView       = (TextView) findViewById(R.id.price_text_view);
+        orderSummaryTextView = (TextView) findViewById(R.id.order_summary_text_view);
 
+    }
+
+    private String createOrderSummary(){
+        return new String(
+                "Name: " + "Kaptain Kunal" + "\n" +
+                "Quantity: " + quantity + "\n" +
+                "Total: " + NumberFormat.getCurrencyInstance().format(price) + "\n" +
+                "Thank you!" );
     }
 
     public void submitOrder(View view){
-        price = (double) quantity * PRICE_PER_COFFE;
-        displayMessage(price);
+        calculatePrice();
+        displayMessage(createOrderSummary());
     }
 
-    private void display(int number){
+    private void calculatePrice(){
+        price = quantity*PRICE_PER_COFFE;
+    }
+
+    private void displayQuantity(int number){
         quantityTextView.setText("" + number);
     }
 
-    private void displayMessage(double price){
-        priceTextView.setText("Total " + NumberFormat.getCurrencyInstance().format(price) + "\nThank You!");
+    private void displayMessage(String message){
+        orderSummaryTextView.setText(message);
     }
 
     public void increment(View view){
         quantity++;
-        display(quantity);
+        displayQuantity(quantity);
     }
 
     public  void decrement(View view){
         if (quantity > 1)
             quantity--;
-        display(quantity);
+        displayQuantity(quantity);
     }
 }
